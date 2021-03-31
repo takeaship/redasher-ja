cfb42e374851dffd1c30ed388ab552463cc73736
David García Garzón <david.garcia@somenergia.coop>
2021-03-31 14:55:46 +0200

    cli

---
+++ redash_gitstudio/cli.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+__version__ = '0.1'
+
+import click
+from yamlns import namespace as ns
+from .redash import Redash
+from consolemsg import out, step
+from pathlib import Path
+
+
+config = ns.load("config.yaml")
+
+@click.group()
+@click.help_option()
+@click.version_option(__version__)
+def cli():
+    'Manages a git controlled and file based version of Redash dashboards'
+
+@cli.command()
+def list():
+    redash = Redash(config.url, config.apikey)
+    for dashboard in redash.dashboards():
+        dashboard = ns(dashboard)
+        out("{}: {} \"{}\"", dashboard.id, dashboard.slug, dashboard.name)
+
+@cli.command()
+@click.argument('id')
+def pull(id):
+    redash = Redash(config.url, config.apikey)
+    dashboard = ns(redash.dashboard(id))
+    dashboard.dump("{}.yaml".format(dashboard.slug))
+    click.echo(dashboard.dump())
+
+
+@cli.command()
+def qlist():
+    redash = Redash(config.url, config.apikey)
+    for query in redash.queries():
+        query = ns(query)
+        print(query.dump())
+        out("{}: \"{}\"", query.id, query.name)
+
+@cli.command()
+def ulist():
+    redash = Redash(config.url, config.apikey)
+    for user in redash.users():
+        user = ns(user)
+        out("{}: \"{}\"", user.id, user.name)
+
+@cli.command()
+@click.argument('id')
+def qpull(id):
+    """Retrieve a query"""
+    redash = Redash(config.url, config.apikey)
+    query = ns(redash.query(id))
+    #query.dump("{}.yaml".format(query.slug))
+    click.echo(query.dump())
+
+
+
+if __name__=='__main__':
+    cli()
+
